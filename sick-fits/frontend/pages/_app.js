17df5afa042707605092601b70f7215547ef9b58
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-02-26 14:53:56 +0000

    Hook up Apollo client React Provider

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,8 +1,10 @@
+import { ApolloProvider } from '@apollo/client';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 import Page from '../components/Page';
 
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 // Listen to Next JS (?) Router events and trigger the progress bar accordingly
 Router.events.on('routeChangeStart', () => {
@@ -17,10 +19,31 @@ Router.events.on('routeChangeError', () => {
   NProgress.done();
 });
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {};
+
+  // this is required for integration with Apollo
+  // withData adds a getInitialProps to every component
+  // I think this is a way to write this in one place
+  // rather than having to add getInitialProps to every
+  // component by hand.
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  pageProps.query = ctx.query;
+
+  return { pageProps };
+};
+
+export default withData(MyApp);
