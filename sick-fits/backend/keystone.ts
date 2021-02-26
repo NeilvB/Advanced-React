b4de8e5bd825650ed8fb6ed64dbb7d9905e5331a
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2021-02-26 10:01:48 +0000

    Add user model and authentication

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,51 @@
+import { createSchema, config } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import 'dotenv/config';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles
+  },
+});
+
+export default withAuth({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseUrl,
+  },
+  lists: createSchema({
+    User,
+  }),
+  ui: {
+    isAccessAllowed: ({ session }) => {
+      console.log(session);
+      return !!session?.data;
+    },
+  },
+  session: withItemData(statelessSessions(sessionConfig), {
+    User: 'id name email',
+  }),
+});
